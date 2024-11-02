04ee32bf46d4981878b4546fcfe7459b089406de
Ashwin <ashwinvx@users.noreply.github.com>
2024-11-02 09:40:14 -0400

    error handling added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // click a button ad display that recipe //
 // the browser needs to display it //
 
-const spoonacularAPIKEY = "a2bb7203e71c48a0ad55821addd45c2e";
+const spoonacularAPIKEY = "a2bb7203e71c48a0ad55821addd45c2e,,,";
 
 
 const getRecipeButton = document.querySelector('#generate-button');
@@ -13,13 +13,24 @@ getRecipeButton.addEventListener('click', function () {
 });
 
 async function generateRecipes() {
-    const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${spoonacularAPIKEY}`);
-    console.log('response-->', response);
-    displaySection.innerHTML = "";
     let newDiv = document.createElement("div");
     let h3 = document.createElement("h3");
-    h3.innerHTML = response.data.recipes[0].title;
-    displaySection.appendChild(h3);
-    newDiv.innerHTML = response.data.recipes[0].summary;
-    displaySection.appendChild(newDiv);
+    try {
+        const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${spoonacularAPIKEY}`);
+        console.log('response-->', response);
+        displaySection.innerHTML = "";
+        h3.innerHTML = response.data.recipes[0].title;
+        displaySection.appendChild(h3);
+        newDiv.innerHTML = response.data.recipes[0].summary;
+        displaySection.appendChild(newDiv);
+    } catch (error) {
+        console.log('error occured-->', error);
+        displaySection.innerHTML = "";
+        h3.innerHTML = "Error";
+        h3.classList.add("error-response");
+        displaySection.appendChild(h3);
+        newDiv.innerHTML = error.response.data.message;
+        newDiv.classList.add("error-response");
+        displaySection.appendChild(newDiv);
+    }
 }
